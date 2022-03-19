ae315ae825c131e4743b363de70995a4fcf06be8
MikhailShokun <minishokun@gmail.com>
2022-03-19 10:56:13 +0200

    React JS63 lesson  lesson DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,28 @@
+import * as axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '69c2662e-0c7a-4b1d-bb8b-056d1690132d'
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 5) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+}
+
+export const followAPI = {
+    getUsers(currentPage = 1, pageSize = 5) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+}
